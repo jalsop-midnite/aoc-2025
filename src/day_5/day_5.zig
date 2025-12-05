dba34126323dbe61e6a7a5d5c7b4f7051e4b66bc
jalsop-midnite <187285448+jalsop-midnite@users.noreply.github.com>
2025-12-05 22:51:05 +0000

    day_5 part 2

--- src/day_5/day_5.zig
+++ src/day_5/day_5.zig
@@ -42,47 +42,41 @@ pub fn main(args: *std.process.ArgIterator) !void {
     };
     std.debug.print("Id data\n{s}\n", .{ids_data});
 
-    var ranges = std.ArrayList(Range).empty;
+    var ranges = AggregatedRanges{};
     defer ranges.deinit(allocator);
 
-    var range_iter = std.mem.splitSequence(u8, range_data, "\n");
-    while (range_iter.next()) |lower_upper| {
+    var range_data_iter = std.mem.splitSequence(u8, range_data, "\n");
+    while (range_data_iter.next()) |lower_upper| {
         var bounds = std.mem.splitSequence(u8, lower_upper, "-");
-        const lower = std.fmt.parseInt(u64, bounds.next().?, 10) catch |err| {
-            std.debug.print("Invalid lower bound: {s}\n", .{lower_upper});
-            return err;
-        };
+
+        const lower = try std.fmt.parseInt(u64, bounds.next().?, 10);
         const upper = try std.fmt.parseInt(u64, bounds.next().?, 10);
 
         std.debug.print("Add range {d} - {d}\n", .{ lower, upper });
-        try ranges.append(
-            allocator,
-            Range{
-                .start = lower,
-                .end = upper,
-            },
-        );
+        const range = Range{
+            .start = lower,
+            .end = upper,
+        };
+
+        try ranges.addRange(allocator, range);
     }
 
     var fresh_ids: usize = 0;
-    var id_iter = std.mem.splitSequence(u8, ids_data, "\n");
-    while (id_iter.next()) |id_string| {
-        if (id_string.len == 0) continue;
+    var end: u64 = 0;
 
-        std.debug.print("Check id: {s}\n", .{id_string});
-        const id = try std.fmt.parseInt(u64, id_string, 10);
+    std.debug.print("Counting fresh ids:\n", .{});
+    var range_iter = ranges.iter();
+    while (range_iter.next()) |range_node| {
+        const range = &range_node.range;
 
-        var is_fresh = false;
-        for (ranges.items) |range| {
-            if (range.start <= id and id <= range.end) {
-                is_fresh = true;
-                break;
-            }
-        }
+        std.debug.assert(end < range.start - 1);
 
-        if (is_fresh) {
-            fresh_ids += 1;
-        }
+        end = range.end;
+
+        std.debug.print("Aggregated range {d} - {d}\n", .{ range.start, range.end });
+        std.debug.print("  Count: {d}\n", .{range.end - range.start + 1});
+
+        fresh_ids += @as(usize, range.end - range.start + 1);
     }
 
     try aoc_2025.output("{d}\n", .{fresh_ids});
@@ -92,3 +86,183 @@ const Range = struct {
     start: u64,
     end: u64,
 };
+
+const AggregatedRanges = struct {
+    nodes: std.DoublyLinkedList = .{},
+
+    const RangeNode = struct {
+        range: Range,
+        node: std.DoublyLinkedList.Node,
+    };
+
+    const Iter = struct {
+        current_node: ?*std.DoublyLinkedList.Node,
+
+        pub fn next(self: *Iter) ?*RangeNode {
+            if (self.current_node) |current_node| {
+                // Intrusive data structure magic which is now built-in
+                const range_node = @as(*RangeNode, @fieldParentPtr("node", current_node));
+                self.current_node = current_node.next;
+                return range_node;
+            } else {
+                return null;
+            }
+        }
+    };
+
+    pub fn deinit(self: *AggregatedRanges, allocator: std.mem.Allocator) void {
+        while (self.nodes.first) |node| {
+            self.remove(
+                allocator,
+                @fieldParentPtr("node", node),
+            );
+        }
+    }
+
+    pub fn iter(self: *const AggregatedRanges) Iter {
+        return .{
+            .current_node = self.nodes.first,
+        };
+    }
+
+    pub fn addRange(self: *AggregatedRanges, allocator: std.mem.Allocator, range: Range) !void {
+        const new_range = try allocator.create(RangeNode);
+        new_range.range = range;
+        new_range.node = .{};
+
+        if (self.nodes.first == null) {
+            // First range being added
+            self.nodes.append(&new_range.node);
+            return;
+        }
+
+        var iterator = self.iter();
+        var inserted = false;
+        while (iterator.next()) |range_node| {
+            const existing_range = &range_node.range;
+
+            // Maintain order of ranges using start value when adding new range
+            if (range.start > existing_range.start) {
+                continue;
+            }
+
+            self.nodes.insertBefore(&range_node.node, &new_range.node);
+            inserted = true;
+            break;
+        }
+
+        // Handle the case where the new range is the largest and goes at the end
+        if (!inserted) {
+            self.nodes.append(&new_range.node);
+        }
+
+        self.aggregate(allocator);
+    }
+
+    fn aggregate(self: *AggregatedRanges, allocator: std.mem.Allocator) void {
+        // Make sure adjacent ranges are merged after adding a new range
+
+        var left = self.nodes.first;
+        while (left) |left_node| {
+            const left_range: *RangeNode = @fieldParentPtr("node", left_node);
+
+            if (left_node.next) |right_node| {
+                const right_range: *RangeNode = @fieldParentPtr("node", right_node);
+
+                if (left_range.range.end + 1 >= right_range.range.start) {
+                    // Merge together by extending left range and removing right range
+                    left_range.range.end = @max(left_range.range.end, right_range.range.end);
+                    self.remove(allocator, right_range);
+                    // Stay on the same left node to check for further merges
+                    continue;
+                }
+
+                left = right_node;
+            } else {
+                break;
+            }
+        }
+    }
+
+    fn remove(self: *AggregatedRanges, allocator: std.mem.Allocator, range_node: *RangeNode) void {
+        self.nodes.remove(&range_node.node);
+        allocator.destroy(range_node);
+    }
+};
+
+test "memory cleanup" {
+    const allocator = std.testing.allocator;
+    var ranges = AggregatedRanges{};
+
+    try ranges.addRange(allocator, .{ .start = 1, .end = 5 });
+
+    ranges.deinit(allocator);
+}
+
+test "add ranges maintains order" {
+    const allocator = std.testing.allocator;
+    var ranges = AggregatedRanges{};
+    defer ranges.deinit(allocator);
+
+    try ranges.addRange(allocator, .{ .start = 5, .end = 10 });
+    try ranges.addRange(allocator, .{ .start = 18, .end = 20 });
+    try ranges.addRange(allocator, .{ .start = 12, .end = 15 });
+
+    var iter = ranges.iter();
+
+    const range1 = iter.next().?;
+    try std.testing.expectEqual(5, range1.range.start);
+    try std.testing.expectEqual(10, range1.range.end);
+
+    const range2 = iter.next().?;
+    try std.testing.expect(range2.range.start == 12);
+    try std.testing.expect(range2.range.end == 15);
+
+    const range3 = iter.next().?;
+    try std.testing.expect(range3.range.start == 18);
+    try std.testing.expect(range3.range.end == 20);
+}
+
+test "aggregation" {
+    const allocator = std.testing.allocator;
+    var ranges = AggregatedRanges{};
+    defer ranges.deinit(allocator);
+
+    // should merge to 1-7
+    try ranges.addRange(allocator, .{ .start = 1, .end = 5 });
+    try ranges.addRange(allocator, .{ .start = 4, .end = 7 });
+
+    // should merge to 9-14
+    try ranges.addRange(allocator, .{ .start = 9, .end = 11 });
+    try ranges.addRange(allocator, .{ .start = 12, .end = 14 });
+
+    // should merge to 20-35
+    try ranges.addRange(allocator, .{ .start = 20, .end = 24 });
+    try ranges.addRange(allocator, .{ .start = 30, .end = 35 });
+    try ranges.addRange(allocator, .{ .start = 23, .end = 31 });
+
+    {
+        var iter = ranges.iter();
+
+        const range1 = iter.next().?;
+        try std.testing.expectEqual(1, range1.range.start);
+        try std.testing.expectEqual(7, range1.range.end);
+
+        const range2 = iter.next().?;
+        try std.testing.expectEqual(9, range2.range.start);
+        try std.testing.expectEqual(14, range2.range.end);
+
+        const range3 = iter.next().?;
+        try std.testing.expectEqual(20, range3.range.start);
+        try std.testing.expectEqual(35, range3.range.end);
+    }
+
+    {
+        try ranges.addRange(allocator, .{ .start = 7, .end = 21 });
+        var iter = ranges.iter();
+
+        const full_range = iter.next().?;
+        try std.testing.expectEqual(1, full_range.range.start);
+        try std.testing.expectEqual(35, full_range.range.end);
+    }
+}
